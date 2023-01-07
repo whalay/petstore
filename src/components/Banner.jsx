1deffbd8031636dae96b50b9b164751c0f3fdee0
Whalay <soliuy5@gmail.com>
2023-01-07 12:53:06 +0100

    added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ReactComponent as Play } from "../assets/images/play.svg";
-import horizontal from "../assets/images/horizontal.svg";
+// import horizontal from "../assets/images/horizontal.svg";
 
 
 const Banner = () => {
@@ -29,7 +29,7 @@ const Banner = () => {
       </div>
       <div className=" w-full md:bg-bg-[#002A48] z-10 flex justify-center">
         <img src="src/assets/images/horizontal.svg" alt="" className=" object-fill md:h-[73vh]" />
-   
+        <img src="src/assets/images/herorec.svg" alt="" className=" object-fill md:h-[73vh]" />
       </div>
     </div>
   );
